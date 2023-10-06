01efef8a603c6b6c930ac0ebe7b0c54a7a33cbef
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-06 17:10:19 -0300

    Add Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
-import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 import { Providers } from './providers'
 import StyledComponentsRegistry from '@/lib/registry'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Boilerplate',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <StyledComponentsRegistry>
           <Providers>{children}</Providers>
         </StyledComponentsRegistry>
