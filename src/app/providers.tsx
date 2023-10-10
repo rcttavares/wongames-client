c6eadbe28d0f82707e06da82d0214b3e75c91574
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-10 17:17:01 -0300

    Add ThemeProvider and styled-components.d.ts

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 import GlobalStyles from '@/styles/global'
 import { PropsWithChildren } from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
