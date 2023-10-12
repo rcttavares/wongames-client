5f44f28b51b558ec3bb986ec5fef2e039c3d0560
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-12 12:08:35 -0300

    Add component test

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
+import Logo, { LogoProps } from '.'
 import { Meta, StoryObj } from '@storybook/react'
-import Logo from '.'
 
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<LogoProps> = {}
