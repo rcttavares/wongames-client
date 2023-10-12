3112e6b06823b932e5b9e2e351fadd7bd51216e5
Rafael Tavares <rafael.tawares@gmail.com>
2023-10-12 11:48:25 -0300

    Config in tsconfig by removing @/*

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Providers } from './providers'
-import StyledComponentsRegistry from '@/lib/registry'
+import StyledComponentsRegistry from 'lib/registry'
 
 export const metadata: Metadata = {
   title: 'Boilerplate',
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import GlobalStyles from '@/styles/global'
+import GlobalStyles from 'styles/global'
 import { PropsWithChildren } from 'react'
 import { ThemeProvider } from 'styled-components'
-import theme from '@/styles/theme'
+import theme from 'styles/theme'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
